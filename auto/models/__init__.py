82fcd13d4ed3aab1cee738e0a6d21930479c8e00
Andrey <165277208+twek366@users.noreply.github.com>
2026-01-28 18:27:52 +0300

    Update __init__.py

--- auto/models/__init__.py
+++ auto/models/__init__.py
@@ -1,19 +1,1 @@
-from .auth import CurrentUserDto, UserAuthDto, UserLoginDto
-from .client import ClientCreateDto, ClientReadDto
-from .client_contact import ClientContactCreateDto, ClientContactReadDto
-from .common import CityReadDto, DealerReadDto, ErrorResponse, Metadata
-from .order import OrderCreateDto, OrderEditDto, OrderListElement, OrderReadDto
-from .order_service import OrderServiceCreateDto, OrderServiceReadDto
-from .pagination import PageResponseClientReadDto, PageResponseOrderListElement
-from .user import UserEditDto, UserReadDto, UserRegisterDto
 
-__all__ = [
-    "UserLoginDto", "UserRegisterDto", "CurrentUserDto", "UserAuthDto",
-    "UserReadDto", "UserEditDto",
-    "OrderCreateDto", "OrderReadDto", "OrderEditDto", "OrderListElement",
-    "OrderServiceCreateDto", "OrderServiceReadDto",
-    "ClientCreateDto", "ClientReadDto",
-    "ClientContactCreateDto", "ClientContactReadDto",
-    "ErrorResponse", "Metadata", "CityReadDto", "DealerReadDto",
-    "PageResponseOrderListElement", "PageResponseClientReadDto",
-]
