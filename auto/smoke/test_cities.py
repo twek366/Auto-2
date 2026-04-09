43946e96f35ef90e53b53b221882ed00f51029f8
Andrey <165277208+twek366@users.noreply.github.com>
2026-04-09 15:14:32 +0300

    Update test_cities.py

--- auto/smoke/test_cities.py
+++ auto/smoke/test_cities.py
@@ -1,68 +1,37 @@
 import allure
 import pytest
+from faker import Faker
 
-from generators.cities_data import update_city
-from helpers.cities import assert_city_response
-from logging_config import logger
+from WS.helpers.cities import get_dto
 
-
-@allure.epic("Controller cities")
-@allure.feature("Создание города")
-@allure.story("POST /cities")
-@allure.severity(allure.severity_level.NORMAL)
-@pytest.mark.smoke
-def test_create_city(city):
-    """Проверка создания города"""
-    assert city["id"] is not None
-    logger.info(f"Город успешно создан: {city['name']}")
-
+fake = Faker("ru_RU")
+name = f"Test {fake.city()}"
 
 
 @allure.epic("Controller cities")
-@allure.feature("Получение города")
-@allure.story("GET /cities/{id}")
-@allure.severity(allure.severity_level.NORMAL)
 @pytest.mark.smoke
-def test_get_city(city):
-    """Получение города по ID"""
-    resp = city["api"].get_by_id(city["id"])
-    assert_city_response(resp, city["name"])
+class TestCities:
 
-    logger.info(f"Город успешно получен: {city['name']}")
+    @allure.story("POST /cities")
+    def test_create_city(self, city_factory):
+        """Проверка создания города"""
+        city_dto = city_factory(name=f"{name}")
 
+        # Создаём город, проверяем что имя соответствует заданному
+        assert city_dto.name == name
 
+    @allure.story("PUT /cities/{id}")
+    def test_update_client(self, city):
+        """Проверка изменения города"""
+        resp = city.api.update(city.id, {"name": name})
 
-@allure.epic("Controller cities")
-@allure.feature("Обновление города")
-@allure.story("PUT /cities/{id}")
-@allure.severity(allure.severity_level.NORMAL)
-@pytest.mark.smoke
-def test_update_city(city):
-    """Обновление города"""
-    resp, new_name = update_city(city["api"], city["id"])
+        # Вносим изменения в город, проверяем наличие внесенных изменений
+        updated_dto = get_dto(resp)
+        assert updated_dto.name == name
 
-    assert resp.status_code == 200
-    assert resp.json()["name"] == new_name
-
-    logger.info(
-        f"Город {city['id']} успешно обновлён: {city['name']} → {new_name}"
-    )
-
-
-
-@allure.epic("Controller cities")
-@allure.feature("Удаление города")
-@allure.story("DELETE /cities/{id}")
-@allure.severity(allure.severity_level.NORMAL)
-@pytest.mark.smoke
-def test_delete_city(city):
-    """Удаление города"""
-    delete_resp = city["api"].delete_by_id(city["id"])
-    assert delete_resp.status_code == 204
-
-    city["deleted"] = True
-
-    get_after_delete = city["api"].get_by_id(city["id"])
-    assert get_after_delete.status_code == 404
-
-    logger.info(f"Город успешно удалён: {city['name']}")
+    @allure.story("DELETE /cities/{id}")
+    def test_delete_client(self, city):
+        """Проверка удаления города через эндпоинт"""
+        # Удаляем созданный город, смотри что контент отсутствует
+        assert city.api.delete_by_id(city.id).status_code == 204
+        assert city.api.get_by_id(city.id).status_code == 404
