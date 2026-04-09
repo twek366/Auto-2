aa4da03950bedd82652d5be3a11518fe579f7bb4
Andrey <165277208+twek366@users.noreply.github.com>
2026-04-09 15:12:43 +0300

    Update city_fixtures.py

--- auto/fixtures/city_fixtures.py
+++ auto/fixtures/city_fixtures.py
@@ -1,67 +1,42 @@
-import json
-
 import allure
 import pytest
 
-from api.cities_api import CitiesApi
-from helpers.cities import *
-from logging_config import logger
+from WS.api.cities_api import CitiesApi
+from WS.generators.cities_data import generate_city_payload
+from WS.helpers.cities import get_dto
 
 
 @pytest.fixture
 def city_factory(admin_token):
-
-    api = CitiesApi(token=admin_token)
+    api = CitiesApi(admin_token)
     created_cities = []
 
-    def _create_city(name: str | None = None):
-        city_name = name or generate_unique_city_name()
-
-        with allure.step(f"Создание города: {city_name}"):
-            payload = create_city_payload(city_name)
+    def _factory(cleanup=True, **overrides):
+        with allure.step(f"Создание тестового города {overrides.get('name', '')}"):
+            payload = generate_city_payload(**overrides)
 
             allure.attach(
-                json.dumps(payload, indent=2),
-                name="Create city payload",
+                str(payload),
+                name="City Payload",
                 attachment_type=allure.attachment_type.JSON,
             )
 
             resp = api.create(payload)
+            dto = get_dto(resp)
+            dto.__dict__["api"] = api
 
-            allure.attach(
-                resp.text,
-                name="Create city response",
-                attachment_type=allure.attachment_type.JSON,
-            )
+            if cleanup:
+                created_cities.append(dto)
+            return dto
 
-            data = assert_city_response(resp, city_name)
+    yield _factory
 
-        city_data = {
-            "id": data["id"],
-            "name": city_name,
-            "api": api,
-            "deleted": False,
-        }
-
-        created_cities.append(city_data)
-        return city_data
-
-    yield _create_city
-
-    for city in created_cities:
-        if not city["deleted"]:
-            with allure.step(f"Удаление тестового города id={city['id']}"):
-                delete_resp = api.delete_by_id(city["id"])
-
-                logger.info(
-                    f"City delete id={city['id']}, status={delete_resp.status_code}"
-                )
-
-                allure.attach(
-                    f"Status: {delete_resp.status_code}\n{delete_resp.text}",
-                    name="Delete city response",
-                    attachment_type=allure.attachment_type.TEXT,
-                )
+    if created_cities:
+        with allure.step(
+            f"Очистка данных: удаление городов ({len(created_cities)} шт.)"
+        ):
+            for city in reversed(created_cities):
+                api.delete_by_id(city.id)
 
 
 @pytest.fixture
